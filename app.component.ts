dd5cb9ea133045ad2ee50e4676aa0a515f524125
Ankit Kumar <37155228+ankit15597@users.noreply.github.com>
2020-04-26 20:01:55 +0530

    Add files via upload
    Three types of data binding have been used in this app.
    1.Two way data binding
    2.Property binding
    3. Event binding
    - Initially when the page is loaded, the reset button is disabled, since the input field is empty.

---
+++ app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  username = 'Ankit';
+  reset(){
+    this.username='';
+  }
+  status(){
+    if(this.username==''){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+}
